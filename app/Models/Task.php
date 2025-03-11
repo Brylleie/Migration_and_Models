abb438c155325200450cfe07b89317f5915d0a9c
Kim Padua <153763511+Brylleie@users.noreply.github.com>
2025-03-11 14:25:28 +0800

    Initial Commit

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,35 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Task extends Model
 {
     protected $fillable = [
-        'name',
+        'title',
         'description',
         'is_completed'
     ];
-
-    public function addTask($record) {
-        return DB::transaction(function() use ($record) {
-            return self::create($record);
-        });
-    }
-
-    public function updateTask($id, $record) {
-        return DB::transaction(function() use ($record, $id) {
-            return $this->where('id', $id)->update([
-                'name' => $record['name'],
-                'description' => $record['description'],
-                'is_completed' => $record['is_completed'],
-            ]);
-        });
-    }
-
-    public function deleteTask($id) {
-        return DB::transaction(function() use ($id) {
-            return $this->where('id', $id)->delete();
-        });
-    }
 }
